ef302bcdff84c496c1dcd158b48eead5d816b9fb
Leonel <leonel-nascimento@hotmail.com>
2024-05-05 21:05:24 +0100

    Fixed the title and nav bottom spacing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,29 +22,33 @@ import { MatButtonModule } from '@angular/material/button';
     h1 {
       font-weight: 500;
     }
-    ul {
-      display: flex;
-      column-gap: .75rem;
-      list-style-type: none;
-    }
     section {
       border: 1px solid gray;
       padding: .5rem;
     }
-    nav a {
-      text-decoration: none;
-      font-size: 1rem;
-      color: rgb(0 0 0 / .5);
+    nav {
+      margin-bottom: .25rem;
 
-      &.active{
-        color: #3366cc;
-        font-weight: 500;
+      ul {
+        display: flex;
+        column-gap: .75rem;
+        list-style-type: none;
+
+        a {
+          text-decoration: none;
+          font-size: 1rem;
+          color: rgb(0 0 0 / .5);
+
+          &.active{
+            color: #3366cc;
+            font-weight: 500;
+          }
+        }
       }
     }
   `
 })
 export class AppComponent {
-  title = 'form-handling';
 
   links = [
     { text: 'Template-Driven', path: '' },
